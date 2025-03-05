8ef1e9c9358a8836f1f2f49a9842fb38e6c1fd80
ana.montero <ana.montero@iesnervion.es>
2025-03-05 18:14:31 +0100

    commit 1

---
+++ src/entornos/pr1.java
@@ -0,0 +1,33 @@
+package entornos;
+
+public class pr1 {
+
+	/**
+	 * Atributo privado para el nombre
+	 */
+	private String nombre = "";
+
+	/**
+	 * Atributo privado para el dni
+	 */
+	private String dni = "";
+
+	/**
+	 * Atributo privado para la edad
+	 */
+	private int edad;
+
+	/**
+	 * <h1>Método para sumar dos parámetros</h1>
+	 * 
+	 * @param a
+	 * @param b
+	 * @return la suma de los dos parámetros
+	 * @author USUARIO
+	 * 
+	 */
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+
+}
